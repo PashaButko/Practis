a3e4dc7f58235cb8d946667e4d8b842c25c6fced
Paul <->
2020-12-01 02:18:45 +0300

    01.12

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -32,5 +32,55 @@ window.addEventListener('DOMContentLoaded', function(){
                 }
             }
         }
-    })
+
+    });
+
+    //timer 
+
+    let deadLine = '2020-12-01';
+
+    function getTimeRemaining(endTime){
+        let t = Date.parse(endTime) - Date.parse(new Date());
+        let seconds = Math.floor((t/1000) % 60);
+        let minutes = Math.floor((t/1000/60) % 60);
+        let hours = Math.floor((t/(1000*60*60)));
+        // let hours = Math.floor((t/1000/60/60) % 24); 
+        // let days = Math.floor((t/(1000*60*60*24)));
+        // таймер дней
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds,
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id);
+        let hours = timer.querySelector('.hours');
+        let minutes = timer.querySelector('.minutes');
+        let seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            function addZero(num){
+                if (num <= 9) {
+                    return '0' + num;
+                } else return num;
+            }
+
+            if (t.total <= 0){
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+                clearInterval(timeInterval);
+            } 
+        }
+    }
+    setClock('timer', deadLine);
 });
