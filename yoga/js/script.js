23821cc08ff6d5f297991059260cccca52da1a4a
Paul <->
2020-12-04 02:52:20 +0300

    2 chapter is over

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -222,6 +222,26 @@ window.addEventListener('DOMContentLoaded', function(){
     descrBtn.addEventListener('click', function(){
         overlay.style.display = 'block';
     });
+
+    class Options{
+        constructor(height, width, bg, fontSize, textAlign){
+            this.height = height;
+            this.width = width;
+            this.bg = bg;
+            this.fontSize = fontSize;
+            this.textAlign = textAlign;
+        }
+
+        createDiv(){
+            let div = document.createElement('div');
+            document.body.appendChild(div);
+            let param = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px; text-align: ${this.textAlign}`;
+            div.style.cssText = param;
+        }
+    }
+
+    const item = new Options(300, 350, "grey", 14, "center");
+    item.createDiv();
 });
 
 // let age = document.getElementById('age');
